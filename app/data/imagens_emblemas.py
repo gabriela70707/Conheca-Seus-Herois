07a52c5be1538361e51eb661adb3f929af980e39
gabriela70707 <gabrielasantos70707@gmail.com>
2025-07-09 15:47:51 -0300

    Adicionando registros no banco de dados

---
+++ app/data/imagens_emblemas.py
@@ -0,0 +1,8 @@
+IMAGENS_EMBLEMAS= [
+    {"nome": "Amor", "url": "http://localhost:8000/static/personagens/personagem2.jpg"},
+    {"nome": "Sabedoria", "url": "http://localhost:8000/static/personagens/personagem3.jpg"},
+    {"nome": "Coragem", "url": "http://localhost:8000/static/personagens/personagem4.jpg"},
+    {"nome": "Fidelidade", "url": "http://localhost:8000/static/personagens/personagem5.jpg"},
+    {"nome": "Paciencia", "url": "http://localhost:8000/static/personagens/personagem6.jpg"},
+    {"nome": "Obediencia", "url": "http://localhost:8000/static/personagens/personagem7.jpg"},
+]
